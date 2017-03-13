75f9e27bf7ee5d99567cfe8ea28e6b25b3258cc1
Souparni Agnihotri <Souparni Agnihotri>
2017-03-12 20:49:39 -0500

    Trying to add specified urls into array list

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WikiCrawler {
 
@@ -25,53 +26,81 @@ public class WikiCrawler {
 		max = max;
 		fileName = fileName;
 	}
+
 	/**
-	 * Find required wikilinks from the given doc
-	 * Change it to return a list of all the required URL's This method can be merged in with the extract links method. 
+	 * Find required wikilinks from the given doc Change it to return a list of
+	 * all the required URL's This method can be merged in with the extract
+	 * links method.
+	 * 
 	 * @param doc
 	 * @return
 	 * @throws IOException
 	 */
-	
-	private ArrayList<String> extractLinks(String doc) throws IOException {
+
+	public ArrayList<String> extractLinks(String doc) throws IOException {
+		//lol right now its getting the link we are NOT supposed to extract. Gotta understand what pattern and matcher does
 		new_doc = after_p(doc);
-		return null;
+		ArrayList<String> links = new ArrayList<String>();
+		String html = new_doc;
+		String regex = "<a href\\s?=\\s?\"([^\"]+)\">";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(html);
+		int index = 0;
+		while(matcher.find(index)){
+			String wholething = matcher.group(); // includes "<a href" and ">"
+				String link = matcher.group(1);
+				links.add(link);
+				//index++;
+
+
+		        index = matcher.end();
+		}
+		
+		return links;
 
 	}
+
 	/**
 	 * 
 	 */
 	private void crawl() {
 
 	}
-	
+
 	/**
-	 * Gets the string beyond the <p> point
+	 * Gets the string beyond the
+	 * <p>
+	 * point
+	 * 
 	 * @param doc
 	 * @return
 	 * @throws IOException
 	 */
-	
+
 	private String after_p(String doc) throws IOException {
-		
+
 		String modified_doc = "";
-		
+
 		FileReader file = new FileReader(doc);
-		
+
 		try (BufferedReader br = new BufferedReader(file)) {
-			
+
 			String line;
-			
+
 			while ((line = br.readLine()) != null) {
-				
+
 				modified_doc += line;
-				
+
 			}
-			
+
 		}
-	
-		modified_doc = modified_doc.substring(modified_doc.lastIndexOf("<p>") + 3); // Ignore Case?? Test for <P>
-		
+
+		modified_doc = modified_doc.substring(modified_doc.lastIndexOf("<p>") + 3); // Ignore
+																					// Case??
+																					// Test
+																					// for
+																					// <P>
+
 		return modified_doc;
 	}
 
