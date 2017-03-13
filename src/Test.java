aea2c0b4856876b5a0fa2837ad137c97a5d2723a
Souparni Agnihotri <Souparni Agnihotri>
2017-03-12 19:43:59 -0500

    new simple changes

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		WikiCrawler w = new WikiCrawler("/wiki/Computer_Science", 100, "WikiCS.txt");
-		w.after_p("sample.txt");
+		
 		//w.crawl();
 	}
 
--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -5,55 +5,74 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class WikiCrawler {
-	
+
 	String seedUrl;
 	int max;
 	String fileName;
-	String new_doc;
+	String new_doc = "";
+
 	/**
 	 * 
 	 * @param seedUrl
-	 * relative address of the seed url (within Wiki domain)
+	 *            relative address of the seed url (within Wiki domain)
 	 * @param max
-	 * Maximum number pages to be crawled
+	 *            Maximum number pages to be crawled
 	 * @param fileName
-	 * name of a file–The graph will be written to this file
+	 *            name of a file–The graph will be written to this file
 	 */
-	public WikiCrawler(String seedUrl, int max, String fileName){
+	public WikiCrawler(String seedUrl, int max, String fileName) {
 		seedUrl = seedUrl;
 		max = max;
 		fileName = fileName;
 	}
+	/**
+	 * Find required wikilinks from the given doc
+	 * Change it to return a list of all the required URL's This method can be merged in with the extract links method. 
+	 * @param doc
+	 * @return
+	 * @throws IOException
+	 */
 	
-	private ArrayList<String> extractLinks(String doc){
-		
+	private ArrayList<String> extractLinks(String doc) throws IOException {
+		new_doc = after_p(doc);
 		return null;
-		
+
+	}
+	/**
+	 * 
+	 */
+	private void crawl() {
+
 	}
 	
-	private Boolean isWikiLink(String doc){
-		
-		return true;
-	}
+	/**
+	 * Gets the string beyond the <p> point
+	 * @param doc
+	 * @return
+	 * @throws IOException
+	 */
 	
-	//change to private -- TODO
-	public String after_p(String doc) throws IOException{
-		String new_doc = "";
+	private String after_p(String doc) throws IOException {
+		
+		String modified_doc = "";
+		
 		FileReader file = new FileReader(doc);
-		try(BufferedReader br = new BufferedReader(file)){
+		
+		try (BufferedReader br = new BufferedReader(file)) {
+			
 			String line;
-			while((line = br.readLine()) != null){
-				new_doc += line;
+			
+			while ((line = br.readLine()) != null) {
+				
+				modified_doc += line;
+				
 			}
+			
 		}
-		
-		new_doc = new_doc.substring(new_doc.lastIndexOf("<p>") + 3); //Ignore Case?? Test for <P>
-		System.out.println(new_doc);
-		return new_doc;
-	}
 	
-	private void crawl(){
+		modified_doc = modified_doc.substring(modified_doc.lastIndexOf("<p>") + 3); // Ignore Case?? Test for <P>
 		
+		return modified_doc;
 	}
 
 }
