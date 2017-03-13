49980891a2ed6a8751bbb38a955640184fc1acca
Souparni Agnihotri <Souparni Agnihotri>
2017-03-12 19:42:32 -0500

    Reads file and returns string after p

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		WikiCrawler w = new WikiCrawler("/wiki/Computer_Science", 100, "WikiCS.txt");
+		w.after_p("sample.txt");
+		//w.crawl();
+	}
+
+}
--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -1,4 +1,59 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class WikiCrawler {
+	
+	String seedUrl;
+	int max;
+	String fileName;
+	String new_doc;
+	/**
+	 * 
+	 * @param seedUrl
+	 * relative address of the seed url (within Wiki domain)
+	 * @param max
+	 * Maximum number pages to be crawled
+	 * @param fileName
+	 * name of a file–The graph will be written to this file
+	 */
+	public WikiCrawler(String seedUrl, int max, String fileName){
+		seedUrl = seedUrl;
+		max = max;
+		fileName = fileName;
+	}
+	
+	private ArrayList<String> extractLinks(String doc){
+		
+		return null;
+		
+	}
+	
+	private Boolean isWikiLink(String doc){
+		
+		return true;
+	}
+	
+	//change to private -- TODO
+	public String after_p(String doc) throws IOException{
+		String new_doc = "";
+		FileReader file = new FileReader(doc);
+		try(BufferedReader br = new BufferedReader(file)){
+			String line;
+			while((line = br.readLine()) != null){
+				new_doc += line;
+			}
+		}
+		
+		new_doc = new_doc.substring(new_doc.lastIndexOf("<p>") + 3); //Ignore Case?? Test for <P>
+		System.out.println(new_doc);
+		return new_doc;
+	}
+	
+	private void crawl(){
+		
+	}
 
 }
