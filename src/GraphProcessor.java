304ad181f91d8e5e972f9a7e5269bccf30303fe3
LAPTOP-1MJEVISJ\Souparni Agnihotri <souparni@iastate.edu>
2017-03-28 15:18:42 -0500

    Making new class

---
+++ src/GraphProcessor.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class GraphProcessor {
+
+		public GraphProcessor(String graphData){
+			
+		}
+		
+		public String outDegree(String v){
+			return v;
+			
+		}
+		
+		public boolean sameComponent(String u, String v){
+			return true;
+		}
+		
+		public ArrayList<String> componentVertices(String v){
+			return null;
+			
+		}
+		
+		public String largestComponent(){
+			return null;
+			
+		}
+		
+		public int numComponents(){
+			return 0;
+			
+		}
+		
+		public ArrayList<String> bfsPath(String u, String v){
+			return null;
+			
+		}
+}
