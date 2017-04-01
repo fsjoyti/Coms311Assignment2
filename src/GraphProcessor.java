6f2d2702f22f6a2ccc5212c56584701fec95b624
LAPTOP-1MJEVISJ\Souparni Agnihotri <souparni@iastate.edu>
2017-03-31 19:12:10 -0500

    DFS algo

--- src/GraphProcessor.java
+++ src/GraphProcessor.java
@@ -1,119 +1,167 @@
 
-
-
 import java.io.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class GraphProcessor {
 
-		Map<String, List<String>> adjacency_list = new HashMap<String, List<String>>();
-		int numVertices;
-		int numComponents;
-	    /**
-	     * 
-	     * @param graphData
-	     * hold the absolute path of a file that stores a
-	     * directed graph. This file will be of the following format:
-	     * First line indicates number of vertices
-	     * Each subsequent line lists a directed edge of the graph
-	     * @throws FileNotFoundException
-	     */
-		public GraphProcessor(String graphData) throws FileNotFoundException{
-			File f = new File(graphData);
-			Scanner input = new Scanner(f);
-			this.numVertices = (Integer.parseInt(input.next()));
-			while(input.hasNext()){
-				String v  = input.next();
-				String e = input.next();
-				 if(!adjacency_list.containsKey(v)) adjacency_list.put(v, new LinkedList());
-				 adjacency_list.get(v).add(e);
+	LinkedHashMap<String, LinkedHashSet<String>> adjacency_list = new LinkedHashMap<String, LinkedHashSet<String>>();
+	ArrayList<String> dfsOutput;
+	Map<String, Integer> myMap = new HashMap<String, Integer>();
+	int numVertices;
+	int count = 1;
+	String[] vertices;
+	public GraphProcessor(String graphData) throws FileNotFoundException {
+		//vertices = new String[numVertices];
+		int verticesCounter = 0;
+		File f = new File(graphData);
+		Scanner input = new Scanner(f);
+		this.numVertices = (Integer.parseInt(input.next()));
+		vertices = new String[numVertices];
+		
+		while (input.hasNext()) {
+			String v = input.next();
+			String e = input.next();
+			if (!adjacency_list.containsKey(v)){
+				adjacency_list.put(v, new LinkedHashSet<String>());
+				vertices[verticesCounter] = v;
+				verticesCounter++;
+			}
 				
-			}
+			adjacency_list.get(v).add(e);
 			
-			System.out.println(adjacency_list);
-			
-		}
-		
-		/**
-		 * Returns the out degree of v
-		 * @param v
-		 * @return
-		 */
-		public int outDegree(String v){
-			int outDegree = 0;
-			if (adjacency_list.containsKey(v)){
-				LinkedList<String> list = (LinkedList<String>) adjacency_list.get(v);
-				outDegree = list.size();
-			}
-			return outDegree;
-			
-		}
-		
-		/**
-		 * Returns true if u and v belong to the same SCC; otherwise
-		 * returns false.
-		 * @param u
-		 * @param v
-		 * @return
-		 */
-		
-		public boolean sameComponent(String u, String v){
-			return true;
-		}
-		
-		/**
-		 * Return all the vertices that belong to the same Strongly Connected
-		 * Component of v (including v).
-		 * @param v
-		 * @return
-		 */
-		public ArrayList<String> componentVertices(String v){
-			return null;
-			
-		}
-		
-		/**
-		 * Returns the size of the largest component.
-		 * @return
-		 */
-		public String largestComponent(){
-			return null;
-			
-		}
-		
-		/**
-		 *  Returns the number of strongly connect components.
-		 * @return
-		 */
-		public int numComponents(){
-			return numComponents;
-			
-		}
-		
-		/**
-		 * This method returns an array
-		 * list of strings that represents the BFS path from u to v.
-		 * Note that this method must return an array list of Strings. 
-		 * First vertex in the path must be u and the last vertex must be v.
-		 * If there is no path from u to v, then this method returns an empty list
-		 * @param u
-		 * @param v
-		 * @return
-		 * Returns the BFS path from u to v
-		 */
-		public ArrayList<String> bfsPath(String u, String v){
-			
-			return null;
-			
-		}
-		/**
-		 * 
-		 */
-		private void stronglyConnectedComponents(){
-			Deque <String> stack = new ArrayDeque<>();
-			
-			
+
 		}
 
+		System.out.println("OriginalList: " +adjacency_list);
+		//System.out.println("map: " +myMap);
+		//System.out.println("My vertices Array: " +Arrays.asList(vertices));
+	}
+
+	public int outDegree(String v) {
+		
+		int outDegree = 0;
+		if (adjacency_list.containsKey(v)) {
+			// LinkedList<String> list = (LinkedList<String>)
+			// adjacency_list.get(v);
+			LinkedHashSet<String> set = adjacency_list.get(v);
+			outDegree = set.size();
+		}
+		//getReversedGraph(adjacency_list);
+		DFSAlgo(adjacency_list,0);
+		return outDegree;
+
+	}
+
+	public boolean sameComponent(String u, String v) {
+		return true;
+	}
+
+	public ArrayList<String> componentVertices(String v) {
+		return null;
+
+	}
+
+	public String largestComponent() {
+		return null;
+
+	}
+
+	public int numComponents() {
+		return 0;
+
+	}
+
+	public ArrayList<String> bfsPath(String u, String v) {
+		return null;
+
+	}
+
+	private void stronglyConnectedComponents() {
+		Deque<String> stack = new ArrayDeque<>();
+
+	}
+
+	private void computeOrder(LinkedHashMap<String, LinkedHashSet<String>> OrigadjacencyList) {
+
+		//Compute the reversed adjacency list
+		LinkedHashMap<String, LinkedHashSet<String>> revAdjList = getReversedGraph(OrigadjacencyList);
+		
+		//Unmark every vertex v
+		int[] unmarked = new int[numVertices];
+		
+		for(int i = 0; i<unmarked.length; i++){
+			unmarked[i] = 0;
+		}
+		
+		//
+		int counter = 0;
+		
+		
+		
+	}
+	
+	private void DFSAlgo(LinkedHashMap<String, LinkedHashSet<String>> graph,int n){
+		dfsOutput = new ArrayList<String>();
+		//Unmark every vertex
+		boolean[] visited = new boolean[numVertices];
+		//int[] parent = new int[numVertices];
+		//int[] DFSNum = new int[numVertices];
+		
+		DFS(adjacency_list, visited, vertices[n]);		
+		
+		
+	}
+	
+	private void DFS(LinkedHashMap<String, LinkedHashSet<String>> graph, boolean visited[], String v){
+		
+		int counter = 0;
+		visited[counter] = true;
+		System.out.println(v + " ");
+		
+		
+		int newCounter = 0;
+		for(String w : graph.get(vertices[newCounter])){
+			
+			if(!visited[newCounter]){
+				DFS(adjacency_list, visited, w);
+			}
+			System.out.println("dfs visited: " +dfsOutput.add(vertices[newCounter]));
+			newCounter++;
+		}
+		counter++;
+	}
+	
+	
+	
+
+	/**
+	 * Finding the reverse ordering of the edges
+	 * @param adjacencyList
+	 * @return
+	 */
+	public LinkedHashMap<String, LinkedHashSet<String>> getReversedGraph(LinkedHashMap<String, LinkedHashSet<String>> adjacencyListMap) {
+
+		LinkedHashMap<String, LinkedHashSet<String>> revAdjListMap = new LinkedHashMap<String, LinkedHashSet<String>>();
+		
+		for (String key : adjacency_list.keySet()){
+			 HashSet <String> set = adjacency_list.get(key);
+			 Iterator<String> iterator = set.iterator();
+			
+			 while (iterator.hasNext()){
+				 String element = iterator.next();
+				
+				 if(! revAdjListMap.containsKey(element))  revAdjListMap.put(element,new  LinkedHashSet<String>());
+				 revAdjListMap.get(element).add(key);
+			 }
+			 
+			 
+			 
+		}
+		
+		System.out.println("Reversed List is: "  +revAdjListMap);
+		
+		return revAdjListMap;
+
+	}
 }
-
--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,24 @@
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 public class Test {
 
 	public static void main(String[] args) throws IOException,InterruptedException {
 
 
-		WikiCrawler w = new WikiCrawler("/wiki/Complexity_theory", 100, "WikiCS.txt");
+		//WikiCrawler w = new WikiCrawler("/wiki/Complexity_theory", 100, "WikiCS.txt");
 		//System.out.println(w.extractLinks("sample.txt"));
-		w.crawl();
-		System.out.println("Done");
+		//w.crawl();
+		//System.out.println("Done");
 		//w.extractLinks("sample.txt");
 		//System.out.println(w.map());
-		/*GraphProcessor g = new GraphProcessor("wikiCC.txt");
-		System.out.println(g.outDegree("Chicago"));
-		System.out.println(g.outDegree("/wiki/Complexity_theory"));*/
+		GraphProcessor g = new GraphProcessor("sampleGraph.txt");
+		System.out.println("OutDegree is: " +g.outDegree("Chicago"));
+		//System.out.println(g.outDegree("/wiki/Complexity_theory"));
+		//LinkedHashMap<String, LinkedHashSet<String>> newMap = 
+		//System.out.println("Reversed List: " +g.("Chicago"));
+
 
 	}
 
